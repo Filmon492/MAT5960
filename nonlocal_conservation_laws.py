733733febc29a83b08314434b3642021a693aef3
Filmon Misgane <filmon@uio.no>
2025-09-22 17:03:14 +0200

    The flux funtion for the nonlocal is well implemented and works fine

--- nonlocal_conservation_laws.py
+++ nonlocal_conservation_laws.py
@@ -11,27 +11,32 @@ class Nonlocal_Conservation_laws(Local_Conservation_Laws):
         super().__init__(T, x_L, x_R, K, N)
         self.epslon = epslon  
         self.alpha = alpha 
-        self.M = int(self.epslon / self.h)
+        self.M = int(self.epslon / self.h + 1.0)
+        self.q_w = self.quadrature_weights()
     
     def nonlocal_kernel_w(self, y):
         epslon= self.epslon
-        return   2*(epslon-y)/ epslon**2 #np.exp(-y/epslon)/ epslon 
-    
+        return 2*(epslon -y)* epslon**2
+        #return np.exp(-y/epslon)/ epslon 
+        #return np.exp(-y * y / (epslon * epslon))
+
     def quadrature_weights(self):
         q_w= np.zeros(self.M) # quadrature weights given by left endpoint
         for k in range(self.M):
-            q_w[k] = self.nonlocal_kernel_w(k*self.h) * self.h
-        return q_w
+            #q_w[k] = quad(self.nonlocal_kernel_w, k*self.h, min((k+1)*self.h, self.epslon))[0] #self.nonlocal_kernel_w(k*self.h) * self.h
+            q_w[k] = self.nonlocal_kernel_w(k * self.h)
+        return q_w / np.sum(q_w) # / self.h) * self.h cancels out
     
     def approx_velocity(self,U0):
-        q_w= self.quadrature_weights()
         Vj= np.zeros(self.K)
-        
-        for j in range(self.M):
-            vj= 0
+        I = np.zeros(self.M)
+
+        for j in range(self.K):
             for k in range(self.M):
-                vj +=  q_w[k] *U0[j+k]
-            Vj[j]= 1-vj
+                I[k] = self.q_w[k] * U0[min(j + k, self.K - 1)]
+
+            Vj[j]= 1 - np.sum(I)
+
         return Vj 
 
     def nonlocal_solver(self, U0, bt, boundary_condition):
@@ -43,7 +48,6 @@ class Nonlocal_Conservation_laws(Local_Conservation_Laws):
         dt = self.dt
         U1 = np.zeros(len(U0))  # Vector solution at next time step
         plot_data = [U0.copy()]  # Store initial data and the updated data
-        Vj= self.approx_velocity(U0)
         
         for i in range(1, self.N + 1):  
 
@@ -57,9 +61,16 @@ class Nonlocal_Conservation_laws(Local_Conservation_Laws):
             elif boundary_condition == "artificial":
                 pass
 
+            Vj= self.approx_velocity(U0)
+
+            if np.any(np.isnan(Vj)):
+                print("nan")
+
+            print(100 * i / self.N)
+
             for j in range(1, self.K - 1):  # Avoid index out of range
-                F_R = (U0[j]* Vj[j])/2 + (U0[j+1]*Vj[j+1])/2 + alpha/2 * (U0[j+1] -U0[j]) 
-                F_L = (U0[j-1]* Vj[j-1])/2 + (U0[j]*Vj[j])/2 + alpha/2 * (U0[j] -U0[j-1]) 
+                F_R = (U0[j]* Vj[j])/2 + (U0[j+1]*Vj[j+1])/2 + self.alpha / 2 * (U0[j] - U0[j+1]) 
+                F_L = (U0[j-1]* Vj[j-1])/2 + (U0[j]*Vj[j])/2 + self.alpha / 2 * (U0[j-1] - U0[j]) 
                 U1[j] = U0[j] - dt / h * (F_R - F_L) # Correct Lax-Friedrichs formula
             
             plot_data.append(U1.copy())  # Store the result at each time step
@@ -67,24 +78,35 @@ class Nonlocal_Conservation_laws(Local_Conservation_Laws):
 
         return plot_data
 
-u0 = lambda x: 0.4 + 0.4*np.exp(-100*(x-0.5)**2)
-#u0 = lambda x: 0.1 if x < 0.5 else 0.6  # Initial condition
-alpha = 0.0005
-epslon = 0.005
-a = Nonlocal_Conservation_laws(1, 0, 1, 1000, 1000 ,epslon, alpha)
-bx, bt = a.create_mesh()  
-U0 = a.initial_value(u0, bx)
-plot_data = a.nonlocal_solver(U0, bt, "dirichlet")  
+#u0 = lambda x: 0.4 + 0.4*np.exp(-100*(x-0.5)**2)
+u0 = lambda x: 0.1 if x < 0.5 else 0.6  # Initial condition
+#u0 = lambda x: 1 if x < 0 else 0
+alpha = 2.0
+epslon_1 = 0.01
+#epslon_2 = 0.000001
+a = Nonlocal_Conservation_laws(1, 0, 1, 1000, 4000 ,epslon_1, alpha)
+b= Local_Conservation_Laws(1, 0, 1, 1000, 4000)
+bx_a, bt_a = a.create_mesh()
+bx_b , bt_b = b.create_mesh()  
+U0_a = a.initial_value(u0, bx_a)
+U0_b = b.initial_value(u0,bx_b)
+plot_data_1 = a.nonlocal_solver(U0_a, bt_a, "dirichlet")  
+plot_data_2 = b.local_solver(U0_b, bt_b, "dirichlet") 
+
+
+
+
     #print(bt)
 
     # Plotting the result at t = 1
 final_time = 1
-plt.plot(bx, plot_data[-1], label=f'Time={final_time}')  # Plot the final time state
+plt.plot(bx_a, plot_data_1[-1], label=f'nonlocal at t ={final_time}')
+plt.plot(bx_b, plot_data_2[-1], label=f'local at time={final_time}')   # Plot the final time state
 plt.xlabel('x')
 plt.ylabel('Density')
 plt.title('Lax-Friedrichs Method')
 plt.legend()
 plt.show()
-
+#print(a.M)
 """ Neste gang bør jeg sjekke om jeg har riktig scheme for nonlocal flux. 
 sjekke nonlocal parameterene ,m, epslon, aplha dt/h"""  
